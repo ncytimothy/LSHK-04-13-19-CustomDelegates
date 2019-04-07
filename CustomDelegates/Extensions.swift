917b5de85ea8bb7e5b001ed20dca441185aa08c7
Tim Ng <ncy.timothy@gmail.com>
2019-04-07 10:40:13 +0800

    Reorder Controller Files, Add Default Blue and UIButton Extension

--- CustomDelegates/Extensions.swift
+++ CustomDelegates/Extensions.swift
@@ -8,10 +8,18 @@
 
 import UIKit
 
+extension UIButton {
+    convenience init(title: String) {
+        self.init(type: .system)
+        self.setTitle(title, for: .normal)
+    }
+}
+
 extension UIColor {
     
     static let divisor: CGFloat = 255
     static let todayGray = rgb(red: 242, green: 242, blue: 242)
+    static let defaultBlue = rgb(red: 0, green: 122, blue: 239)
     
     static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat = 1) -> UIColor {
         return UIColor(red: red / divisor, green: green / divisor, blue: blue / divisor, alpha: alpha)
